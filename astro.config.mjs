132dcf7e2d031bfa4427966ae406ca3d331bb36d
Aqualux <aqualux1@ymail.com>
2026-09-13 12:52:43 +0200

    Set production site URL to aqualuxmalta.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://www.aqualux.example',
+  site: 'https://www.aqualuxmalta.com',
   output: 'server',
   adapter: vercel(),
   compressHTML: true,
